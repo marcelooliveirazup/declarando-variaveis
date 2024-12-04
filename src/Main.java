2feeae3e8dcc6e8dd4dd74f2a7befac06787ed35
marcelo13oliveira <83042641+marcelo13oliveira@users.noreply.github.com>
2024-12-04 07:11:04 -0300

    Atividade declarando variaveis

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+public static void main(String[] args) {
+    String nome = "Marcelo de Oliveira";
+    int idade = 31;
+    double altura = 1.70;
+    boolean matricula = true;
+    System.out.println("Dados do aluno:\nNome: " + nome + "\nIdade: " + idade + "\nAltura: " + altura + "\nStatus de matricula: " + matricula);
+}
+
+}
+
